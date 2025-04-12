5b6c2a52f7de60e6d51532099d1943611b10e9a5
makako <jairo.carrillo@inacapmail.cl>
2025-04-11 23:01:52 -0400

    Actualizado

---
+++ QUEPRO-3.py
@@ -0,0 +1,8 @@
+ACL = int(input("Ingrese el numero de ACL: "))
+
+if ACL >= 1 and ACL <= 99:
+    print(F"El numero ({ACL}) corresponde a una ACL Estandar")
+elif ACL >= 100 and ACL <= 199:
+    print(F"El numero ({ACL}) corresponde a una ACL Extendida")
+else:
+    print(F"El Numero ({ACL}) no corresponde a una ACL")
---
+++ STEVE-2.py
@@ -0,0 +1,6 @@
+nombre=input("Ingrese su nombre: ")
+apellido=input("Ingrese apellido paterno: ")
+seccion=input("Ingrese codigo de seccion: ")
+sede=input("Ingrese la Sede: ")
+
+print(f"El estudiente {nombre} {apellido} ingreso a inacap {sede} en la seccion {seccion}")
